9f15ad57e6eeca189521650029bedca954f84890
qan <qanitakhaerunissa.qk@gmail.com>
2026-01-09 23:57:03 +0700

    vercel analytics

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/globals.css'
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
